f3b8c143f035e66a3d153e0ee59714dfbf0ac4f4
Inderjeet Sav <indrajeet.sav@lendenclub.com>
2022-12-11 20:03:41 +0530

    added icons

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,12 @@ import "./styles/scss/main.scss";
 import Login from "./containers/Login/Login";
 import {ThemeProvider} from "styled-components";
 import ThemeContext from "./contexts/ThemeContext";
-import {darkTheme, lightTheme} from "./styles/styles/themes";
+import {
+  darkTheme,
+  // lightTheme
+} from "./styles/styles/themes";
 import GlobalStyles from "./styles/styles/Global";
+import * as Unicons from "@iconscout/react-unicons";
 
 const App = () => {
   return (
@@ -11,6 +15,9 @@ const App = () => {
       <ThemeProvider theme={darkTheme}>
         <GlobalStyles />
         <Login />
+        <p style={{color: "#fff"}}>
+          A text <Unicons.UilWhatsapp style={{color: "red"}} />
+        </p>
       </ThemeProvider>
     </ThemeContext>
   );
--- src/containers/Login/Login.tsx
+++ src/containers/Login/Login.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React from "react";
 import {z} from "zod";
 import {Form, useForm} from "../../utils/Forms";
 import {Input} from "../../components/Input";
